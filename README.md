012868e016de14f59859cfa51c87dbd7024b6c45
Lucas <dexterfail@hotmail.com>
2023-01-31 15:45:07 -0300

    ➕ add styled-components and create basic structure

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 <div align="center">
     <img src="https://img.shields.io/badge/react%20-%2320232a.svg?&style=for-the-badge&logo=react&logoColor=%2361DAFB" />
     <img src="https://img.shields.io/badge/TypeScript-007ACC?style=for-the-badge&logo=typescript&logoColor=white"/>
+    <img src="https://img.shields.io/badge/styled--components-DB7093?style=for-the-badge&logo=styled-components&logoColor=white" />
 </div>
 
 ### Prerequisites
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as Styled from "./App.styles";
+import logoImage from "./assets/devmemory_logo.png";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Styled.Container>
+      <Styled.Info>
+        <Styled.LogoLink href="">
+          <img src={logoImage} width="200" alt="" />
+        </Styled.LogoLink>
+
+        <Styled.InfoArea>
+          ...
+        </Styled.InfoArea>
+
+        <button>Reiniciar</button>
+      </Styled.Info>
+      <Styled.GridArea>
+        ...
+      </Styled.GridArea>
+    </Styled.Container>
+  )
 }
 
 export default App;
